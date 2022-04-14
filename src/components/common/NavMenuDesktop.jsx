f3d5db07254e02d95d4940b3d039043c77285e70
bappy3a <sunnobappy@gmail.com>
2022-04-14 22:30:48 +0600

    slider done

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class NavMenuDesktop extends Component {
   render() {
     return (
-      <div>NavMenuDesktop</div>
+      <Fragment>
+        
+      </Fragment>
     )
   }
 }
