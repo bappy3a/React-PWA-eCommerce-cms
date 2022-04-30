ef3f080b06c05ff81b31038ae9aa07c5e5b0e336
bappy3a <sunnobappy@gmail.com>
2022-04-30 16:57:04 +0600

    ok

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -1,4 +1,4 @@
-import axios from 'axios' 
+import axios from 'axios'
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AppUrl from '../api/AppUrl';
@@ -8,43 +8,42 @@ import HomePage from '../pages/HomePage';
 import ProductDetailsPage from '../pages/ProductDetailsPage';
 import PurchasePage from '../pages/PurchasePage';
 import UserLoginPage from '../pages/UserLoginPage';
-  
+
 
 class AppRoute extends Component {
 
 
-constructor(){
+  constructor() {
     super();
-    this.state={
-         user:{}
+    this.state = {
+      user: {}
     }
-}
+  }
 
-componentDidMount(){
-    axios.get(AppUrl.UserData).then((response) => { 
-         this.setUser(response.data)
-    }).catch(error=>{
+  componentDidMount() {
+    axios.get(AppUrl.UserData).then((response) => {
+      this.setUser(response.data)
+    }).catch(error => {
 
     });
-}
+  }
 
 
-setUser = (user) => {
-    this.setState({user:user})
-}
-
+  setUser = (user) => {
+    this.setState({ user: user })
+  }
 
   render() {
     return (
       <Fragment>
-          <Switch>
-            <Route exact path="/" render={(props) => <HomePage {...props} key={Date.now()} /> } />
-            <Route exact path="/login" component={UserLoginPage} ></Route>
-            <Route exact path="/contact" component={ContactPage} ></Route>
-            <Route exact path="/purchase" component={PurchasePage} ></Route>
-            <Route exact path="/productdetails/:code" component={ProductDetailsPage} ></Route>
-            <Route exact path="/cart" component={CartPage} />
-          </Switch>
+        <Switch>
+          <Route exact path="/" render={(props) => <HomePage {...props} key={Date.now()} />} />
+          <Route exact path="/login" component={UserLoginPage} ></Route>
+          <Route exact path="/contact" component={ContactPage} ></Route>
+          <Route exact path="/purchase" component={PurchasePage} ></Route>
+          <Route exact path="/productdetails/:code" component={ProductDetailsPage} ></Route>
+          <Route exact path="/cart" component={CartPage} />
+        </Switch>
       </Fragment>
     )
   }
